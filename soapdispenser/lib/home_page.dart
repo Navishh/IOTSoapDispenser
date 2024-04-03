594209dd0a2c0c4e721c8e77ff5eba9d341d2611
Navishh <navishkanuwan11@gmail.com>
2024-04-03 13:47:04 +0530

    Created soapCount Card

--- soapdispenser/lib/home_page.dart
+++ soapdispenser/lib/home_page.dart
@@ -12,22 +12,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late DatabaseReference _ref;
-  int _humanCount = 0;
+late DatabaseReference _humanCountRef;
+late DatabaseReference _soapCountRef;
+int _humanCount = 0;
+int _soapCount = 0;
 
-    @override
-  void initState() {
-    super.initState();
-    _ref = FirebaseDatabase.instance.reference().child('humanCount');
+@override
+void initState() {
+  super.initState();
+  
+  // Reference for 'humanCount'
+  _humanCountRef = FirebaseDatabase.instance.reference().child('humanCount');
+  
+  // Reference for 'soapCount'
+  _soapCountRef = FirebaseDatabase.instance.reference().child('soapCount');
 
-    _ref.onValue.listen((event) {
-      setState(() {
-        if (event.snapshot.value != null) {
-          _humanCount = event.snapshot.value as int;
-        }
-      });
+  // Listener for 'humanCount'
+  _humanCountRef.onValue.listen((event) {
+    setState(() {
+      if (event.snapshot.value != null) {
+        _humanCount = event.snapshot.value as int;
+      }
     });
-  }
+  });
+
+  // Listener for 'soapCount'
+  _soapCountRef.onValue.listen((event) {
+    setState(() {
+      if (event.snapshot.value != null) {
+        _soapCount = event.snapshot.value as int;
+      }
+    });
+  });
+}
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -97,6 +116,83 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
+
+          
+
+                        //SOAP COUNT CARD
+            Container(
+              height: 200,
+              margin: EdgeInsets.only(left: 10, right: 10, top: 20),
+              child: Card(
+                color: Color(0xFF3FD4C3),
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      const Padding(
+                        padding: EdgeInsets.all(40),
+                        child: Icon(
+                          Icons.water_drop_sharp,
+                          size: 57,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(right: 80, top: 45),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: <Widget>[
+                                Text(
+                                  'COUNT',
+                                  style: GoogleFonts.poppins(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                               Text(
+                                  _humanCount.toString(),
+                                  style: GoogleFonts.poppins(
+                                    color: Colors.white,
+                                    fontSize: 50,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            //REFILL MESSAGE CARD
+            Container(
+              height: 80,
+              margin: EdgeInsets.only(left: 10, right: 10, top: 20),	
+              child: Card(
+                color: Color.fromARGB(255, 5, 241, 52),
+                child: Center(
+                  child: Text(
+                    'Good to go!',
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+
           ],
         ),
       ),
